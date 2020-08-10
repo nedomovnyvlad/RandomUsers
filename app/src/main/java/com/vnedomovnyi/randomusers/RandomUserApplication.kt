6ac338b2aa3d4ca2d30b573645bfdb04f59f83d1
Vladislav Nedomovnyi <nedomovnyvlad@gmail.com>
2020-08-10 08:45:53 +0300

    Enable logging with Timber

--- app/src/main/java/com/vnedomovnyi/randomusers/RandomUserApplication.kt
+++ app/src/main/java/com/vnedomovnyi/randomusers/RandomUserApplication.kt
@@ -1,5 +1,14 @@
 package com.vnedomovnyi.randomusers
 
 import android.app.Application
+import timber.log.Timber
 
-class RandomUserApplication : Application()
+class RandomUserApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+}
