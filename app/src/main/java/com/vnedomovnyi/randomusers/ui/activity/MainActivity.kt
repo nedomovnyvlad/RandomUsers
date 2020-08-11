c2cf63d8a8d4eae1ddc7c13fd9ad28ec1ff2bffe
Vladislav Nedomovnyi <nedomovnyvlad@gmail.com>
2020-08-11 09:30:57 +0300

    Move MainActivity to the activity package

---
+++ app/src/main/java/com/vnedomovnyi/randomusers/ui/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.vnedomovnyi.randomusers.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vnedomovnyi.randomusers.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
